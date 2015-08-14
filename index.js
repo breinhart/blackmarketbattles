f28af2fe6b556e1a5af0af84ed3a7c704ab0dbcd
Brian Reinhart <brian@cocoa-mag.com>
2015-08-13 19:24:53 -0500

    Added web sockets

--- index.js
+++ index.js
@@ -10,11 +10,29 @@ app.set('views', __dirname + '/views');
 app.set('view engine', 'ejs');
 
 app.get('/', function(request, response) {
-  response.render('pages/index');
+    response.render('pages/index');
 });
 
 app.listen(app.get('port'), function() {
-  console.log('Node app is running on port', app.get('port'));
+    console.log('Node app is running on port', app.get('port'));
 });
+
+console.log("http server listening on %d", port)
+
+var wss = new WebSocketServer({server: server})
+console.log("websocket server created")
+
+wss.on("connection", function(ws) {
+    var id = setInterval(function() {
+        ws.send(JSON.stringify(new Date()), function() {  })
+    }, 1000)
+
+    console.log("websocket connection open")
+
+    ws.on("close", function() {
+        console.log("websocket connection close")
+        clearInterval(id)
+    })
+})
 
 
